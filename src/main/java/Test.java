7be9db9cdd507ba8bc44f4ca3061133c6811b6e7
jiatianyu <371238503@qq.com>
2021-01-20 23:33:36 +0800

    分支dev初始化操作结束

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("初始化操作");
+        System.out.println("dev初始化操作");
+        System.out.println("dev初始化操作结束");
     }
 }
